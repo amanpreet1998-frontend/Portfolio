778a7c6e41b8c20f2e2ebc3e880834e68119de18
amanpreet1998-frontend <amandhaliwal1693@gmail.com>
2025-05-20 23:22:24 +0530

    Initial commit

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,38 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import {BrowserRouter,Routes,Route} from "react-router-dom";
+import Home from'./Home'
+import About from'./About'
+import Skills from './Skills'
+import Projects from './Projects'
+import Contact from './Contact'
+import Navbar from './Navbar'
+//import Project1 from './Project1'
+//import { AnimatedBackground } from 'animated-backgrounds';
+
+
+
+function App() {
+ return (
+    <>
+      <BrowserRouter>
+      <Navbar/>
+        <Routes>
+        
+          <Route path="/"element={<Home/>}/>
+          <Route path="/about"element={<About/>}/>
+          <Route path="/skills"element={<Skills/>}/>
+          <Route path="/projects"element={<Projects/>}/>
+          <Route path="/contact"element={<Contact/>}/>
+        </Routes>
+        
+        
+      </BrowserRouter>
+       
+    </>
+  );
+};
+
+export default App
---
+++ vite-project/src/Projects.jsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import {Link} from "react-router-dom";
+import project1 from '../src/assets/project1.png';
+import project2 from '../src/assets/project2.png';
+import Aos from 'aos';
+import 'aos/dist/aos.css';
+import '../src/styles/Projects.css';
+
+const Projects=()=>{
+   Aos.init();
+    return(
+      <>
+        <div className='project'>
+          <div className='project-title'>
+            <center>
+<h1 className='p-5'>Projects</h1>
+</center>
+{/* <img src={project1} alt=''/> */}
+          </div>
+          <div className='project-container'>
+            <div className='project-container-1'>
+            <img data-aos="flip-left"
+     data-aos-easing="ease-out-cubic"
+     data-aos-duration="2000"className='project1' src={project1}/>
+            <h2 className='desc-1'>Urban Food - food ordering website</h2>
+            </div>
+            <div className='project-container-1'>
+            <img data-aos="flip-left"
+     data-aos-easing="ease-out-cubic"
+     data-aos-duration="2000"className='project2' src={project2}/>
+            <h2 className='desc-2'>Portfolio</h2>
+            </div>
+          </div>
+          </div>
+        {/* <div data-aos="fade-up"
+     data-aos-duration="3000">
+<h1>Projects</h1><br/></div>
+<div data-aos="zoom-in-down">
+<p>I have built various different projects to fit different aspects of the client's business.<br/>
+           If you want to see more examples of my work than the ones showcased in this site, please contact me!</p></div>
+           <Link to="/project1">See Projects</Link> */}
+</>
+    )
+}
+
+export default Projects;
