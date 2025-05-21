a30a1e411fffec75b3b2164d7bf14a6f0d099b97
amanpreet1998-frontend <amandhaliwal1693@gmail.com>
2025-05-21 11:34:57 +0530

    Add GitHub Actions workflow and fix GitHub Pages paths

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import viteLogo from '../public/vite.svg'
 import './App.css'
 import {HashRouter, Routes, Route} from "react-router-dom";
 import Home from'./Home'
