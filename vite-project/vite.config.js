67809840cd7a2885d686bcf9b85657d187363491
amanpreet1998-frontend <amandhaliwal1693@gmail.com>
2025-05-25 12:43:26 +0530

    first commit

--- vite-project/vite.config.js
+++ vite-project/vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/Portfolio/',
+   base: '/Portfolio-new/',
   plugins: [
     tailwindcss(),
     react()],
