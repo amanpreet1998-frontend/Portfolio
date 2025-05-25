5d8b2502b3cb9fae41135627e3066791086ef4bd
amanpreet1998-frontend <amandhaliwal1693@gmail.com>
2025-05-25 17:01:52 +0530

    Fix build configuration and update dependencies

--- vite-project/vite.config.js
+++ vite-project/vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
-   base: '/Portfolio-new/',
+   base: '/Portfolio/',
   plugins: [
     tailwindcss(),
     react()],
